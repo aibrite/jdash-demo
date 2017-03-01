680bffd629d7adefb87ca10c56b5a2827ede2f07
Tansu TURKOGLU <tansu@Tansus-MacBook-Pro.local>
2017-03-01 21:28:56 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+var del = require('del');
+var gulp = require('gulp');
+
+var deploydir = './gh-pages';
+
+gulp.task('deploy:clean', [], function (done) {
+    del([
+        deploydir
+    ], {
+            force: true
+        }).then(() => done()).catch(err => done(err))
+})
+
+gulp.task('deploy:xcopy', ['deploy:clean'], function () {
+    gulp.src(['./bootstrap/**', './css/**', './jdash/**', './material/**', './node_modules/jdash-ui/**', './index.html'], {
+        base: './'
+    }).pipe(gulp.dest(deploydir));
+})
+
+gulp.task('deploy:push', ['deploy:xcopy'], function (done) {
+    ghpages.publish(path.join(__dirname, deploydir), done);
+})
+
+gulp.task('deploy', ['deploy:clean', 'deploy:xcopy', 'deploy:push'], function () {
+
+})
