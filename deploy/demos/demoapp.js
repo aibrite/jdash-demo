463c3b8e36d6b8edebb7e5291abc2a58da75ac29
Utku Utku <utku2107@Utkus-MacBook-Air.local>
2017-07-27 22:23:25 +0300

    minor ui fixes

---
+++ deploy/demos/demoapp.js
@@ -0,0 +1,4 @@
+(function(){
+    jdash.Provider = new jdash.ProviderTypes.OnPremise({ url: '/jdash/api/v1' }); 
+    jdash.Provider.init = function(){};
+})();
---
+++ deploy/lib/jdash-ui/push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "new version"
+git push
