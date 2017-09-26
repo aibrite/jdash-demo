1d426ecb1bd5eca7646acd45857fa67bb19fd40e
theutku <turkoglu.utku@gmail.com>
2017-09-26 15:19:29 +0300

    major bs changes

--- deploy/demos/bs/custom.js
+++ deploy/demos/bs/custom.js
@@ -1,13 +1,57 @@
 $(window).ready(function () {
-    var introTut = introJs();
-    introTut.setOptions({
+    var intro = introJs();
+    intro.setOptions({
+        steps: [
+            {
+                intro: "Welcome! This is your first dashboard!"
+            },
+            {
+                element: document.querySelectorAll('.row.dashlet-row').item(0),
+                intro: "These are demo dashlets to try."
+            },
+            {
+                element : document.querySelectorAll('.navbar.navbar-default.navbar-fixed').item(0),
+                intro : 'Last word! Create a new dashboard or try different themes!.'
+            }
+        ], 
         exitOnOverlayClick: false,
         exitOnEsc: false,
         showProgress: true,
         hideNext: true,
         disableInteraction: true,
         doneLabel: 'Start!'
-    }).start().oncomplete(function () {
-        console.log('Done');
-    })
-})
+    });
+    intro.start();   
+});
+
+
+function rightBarInitted(navbar, isResize) {
+    if (!isResize) {
+        app.loadThemes(navbar.querySelector('#themes'));
+        app.dashboardListContainer = navbar.querySelector('#dashboard-list');
+    }
+}
+
+function startNewDashboardIntro() {
+    if (!startNewDashboardIntro.isNewDashboardIntroShown) {
+        var intro = introJs();
+        intro.setOptions({
+            steps: [
+                {
+                    intro: "Your new dashboard has been created and is ready to design!"
+                },
+                {
+                    element : document.querySelector('#dashlet-list'),
+                    intro : 'You can add any dashlet inside.'
+                },
+                {
+                    element: document.querySelector('#layoutedit'),
+                    intro: "Click design button to design dashboard layout."
+                }
+            ]
+        });
+
+        intro.start();
+        startNewDashboardIntro.isNewDashboardIntroShown = true;
+    }
+}
